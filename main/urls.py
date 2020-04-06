7987a89843b59c99599f4c01dc8711544d7f56ff
sirokome19 <sirokome19@yahoo.co.jp>
2020-04-06 23:05:07 +0000

    can listen

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
+app_name = "main"
 
 urlpatterns = [
     path('', views.MainView.as_view(), name='main'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
